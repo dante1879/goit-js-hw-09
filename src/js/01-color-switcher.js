4889a8b380282f1431967d534f4d931eeccdcf2d
dante1879 <sirenko.alex777@gmail.com>
2023-04-26 23:30:02 +0300

    hw9task1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+startBtn = document.querySelector('[data-start]');
+stopBtn = document.querySelector('[data-stop]');
+body = document.querySelector('body');
+let timerId = null;
 
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick)
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeRandomBodyBgColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function onStartClick() {
+    timerId = setInterval(changeRandomBodyBgColor, 1000);
+    startBtn.disabled = true
+}
+
+function onStopClick() {
+    clearInterval(timerId);
+    startBtn.disabled = false
+}
